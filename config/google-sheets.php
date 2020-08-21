510aeeeb77bf319f12b095bcfbca415e5cba71fd
Keunbae Park <ppotpo@gmail.com>
2020-08-21 16:12:41 -0400

    wip

--- config/google-sheets.php
+++ config/google-sheets.php
@@ -1,6 +1,14 @@
 <?php
 
 return [
+    /*
+     * Set access type. Default is online.
+     */
     'access_type' => 'online',
+
+    /*
+     * Set cache token key. We assume after logging in to google, token is stored in cache using this key.
+     * So google client will retrieve a token from cache using key when it is created.
+     */
     'access_token_key' => 'google_sheet_token',
 ];
